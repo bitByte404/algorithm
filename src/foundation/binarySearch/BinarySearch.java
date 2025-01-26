f26fb7b18ec1bf23541f1d6fddbd21eccf64960d
biteByte404 <3302089539@qq.com>
2025-01-26 13:23:51 +0800

    refactor(foundation): 优化二分查找算法实现
    - 使用位运算优化中间索引的计算- 添加新的二分查找实现方法，改进边界处理逻辑
    - 删除无用的代码注释

--- src/foundation/binarySearch/BinarySearch.java
+++ src/foundation/binarySearch/BinarySearch.java
@@ -13,10 +13,29 @@ public class BinarySearch {
         int i = 0;
         int j = a.length - 1;
         while (i <= j) { // 范围内还要
-            int m = (i + j) / 2;
+            int m = (i + j) >>> 1;
             if (target < a[m]) { // 目标在左边
                 j = m - 1;
-            } else if (target > a[m]) { // 目标在右边
+            } else if (a[m] < target) { // 目标在右边
+                i = m + 1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 改动后j表示的是边界，一定不是目标数
+     */
+    public static int binarySearchAlternative(int[] a, int target) {
+        int i = 0;
+        int j = a.length;
+        while (i < j) {
+            int m = (i + j) >>> 1;
+            if (target < a[m]) {
+                j = m;
+            } else if (a[m] > target) {
                 i = m + 1;
             } else {
                 return m;
